a6ed245971036e4f660f7c5723e54cf110c05732
w3bit <fathi.reza@gmail.com>
2019-08-11 22:18:32 +0430

    fix: fix ui bugs, shorter name for some countries

--- projects/mobile-field-lib/src/lib/mobile-field-lib.component.ts
+++ projects/mobile-field-lib/src/lib/mobile-field-lib.component.ts
@@ -25,10 +25,11 @@ import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
               display: flex;
               border: 1px solid #eee;
               border-radius: 5px;
+              direction: ltr;
           }
 
           select {
-              height: 30px;
+              height: 100%;
               width: 100%;
               border: none;
               outline: none;
